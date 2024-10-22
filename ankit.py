469379c485a75cf116bfc379648919751d5c5ad4
AnkitN21 <ankitnegi.asn21@gmail.com>
2024-10-22 16:18:48 +0530

    ths is cahnged

---
+++ ankit.py
@@ -0,0 +1,11 @@
+class rexgal:
+    def __init__(self):
+        pass
+    def my_func(self):
+        return "hi how are you?"
+    
+my_obj = rexgal()
+
+if __name__ == "__main__":
+    print(my_obj.my_func())
+    
